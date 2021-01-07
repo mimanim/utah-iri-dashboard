f9f1ce4a43592060de60b4d86b6818206ad72d5a
Sean McMenamin <16652523+mimanim@users.noreply.github.com>
2021-01-06 19:43:16 -0700

    feat: Initial map setup for Denver

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React, { Component } from "react";
+import MainMap from "@deck.gl/react";
+import React from "react";
 import { hot } from "react-hot-loader";
+import { StaticMap } from "react-map-gl";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello, World </h1>
-      </div>
-    );
-  }
-}
+const INITIAL_VIEW_STATE = {
+  longitude: -104.9903,
+  latitude: 39.7392,
+  zoom: 11,
+  pitch: 0,
+  bearing: 0,
+};
+
+const App = () => (
+  <main className="App">
+    <MainMap controller initialViewState={INITIAL_VIEW_STATE}>
+      <StaticMap
+        mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKEN}
+        width={400}
+        height={400}
+      />
+    </MainMap>
+  </main>
+);
 
 export default hot(module)(App);
